53ec050192c716fcb8e3f9c7cc11c57e3a2b5ba1
Stéphane Mitermite <stephane.mitermite@berger-levrault.com>
2022-01-07 16:59:08 +0100

    Initial commit

---
+++ AspNetCoreODataDependencyInjection/ODataDependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetCoreODataDependencyInjection;
+
+/// <summary>
+/// <see cref="TypeFilterAttribute"/> to enable AspNetCoreOData dependency injection
+/// </summary>
+public class ODataDependencyInjection : TypeFilterAttribute
+{
+    /// <summary>
+    /// Initializes a new instance of <see cref="ODataDependencyInjection"/> class
+    /// </summary>
+    /// <param name="setupAction">the setup config</param>
+    public ODataDependencyInjection(Action<IServiceCollection> setupAction) : base(typeof(ODataDependencyInjectionResourceFilter))
+    {
+        Arguments = new[] { setupAction };
+    }
+}
---
+++ AspNetCoreODataDependencyInjection/ODataDependencyInjectionResourceFilter.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.OData;
+using Microsoft.AspNetCore.OData.Abstracts;
+using Microsoft.AspNetCore.OData.Extensions;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.OData;
+using System.Reflection;
+
+namespace AspNetCoreODataDependencyInjection;
+
+/// <summary>
+/// Resource filter to enable AspNetCoreOData dependency injection
+/// </summary>
+public class ODataDependencyInjectionResourceFilter : IResourceFilter
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ODataDependencyInjectionResourceFilter"/> class
+    /// </summary>
+    /// <param name="oDataOptionsAccessor"><see cref="ODataOptions"/> accessor</param>
+    /// <param name="setupAction">the setup config</param>
+    public ODataDependencyInjectionResourceFilter(IOptions<ODataOptions> oDataOptionsAccessor, Action<IServiceCollection> setupAction)
+    {
+        Services = new Lazy<IServiceProvider>(() =>
+        {
+            var oDataFeatureInterfaceType = typeof(IODataFeature);
+            var defaultContainerBuilderType =
+                oDataFeatureInterfaceType.Assembly.GetType($"{oDataFeatureInterfaceType.Namespace}.DefaultContainerBuilder")!;
+            // var builder = new DefaultContainerBuilder();
+            var builder = (IContainerBuilder)Activator.CreateInstance(defaultContainerBuilderType)!;
+            builder.AddDefaultODataServices();
+            builder.AddService(Microsoft.OData.ServiceLifetime.Singleton, sp => oDataOptionsAccessor.Value.QuerySettings);
+            // builder.AddDefaultWebApiServices();
+            (oDataFeatureInterfaceType.Assembly.GetType($"{oDataFeatureInterfaceType.Namespace}.ContainerBuilderExtensions")!)
+                .GetMethod("AddDefaultWebApiServices")!.Invoke(null, new[] { builder });
+            // setupAction.Invoke(builder.Services);
+            setupAction?.Invoke((IServiceCollection)defaultContainerBuilderType
+                .GetProperty("Services", BindingFlags.Instance | BindingFlags.NonPublic)!.GetValue(builder)!);
+            return builder.BuildContainer();
+        });
+    }
+
+    private Lazy<IServiceProvider> Services { get; }
+
+    /// <inheritdoc/>
+    public void OnResourceExecuting(ResourceExecutingContext context)
+    {
+        var odataQueryOptionsType = typeof(ODataQueryOptions<>);
+        if (!context.ActionDescriptor.Parameters.Any(p => p.ParameterType.IsGenericType && p.ParameterType.GetGenericTypeDefinition() == odataQueryOptionsType))
+        {
+            return;
+        }
+
+        var odataFeature = context.HttpContext.ODataFeature();
+        if (odataFeature.Services == null)
+        {
+            odataFeature.Services = Services.Value;
+        }
+    }
+
+    /// <inheritdoc/>
+    public void OnResourceExecuted(ResourceExecutedContext context)
+    {
+    }
+}
